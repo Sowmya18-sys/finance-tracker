5c0b3e58469dfbafcb47425f090f25ea9d75ad77
Sowmya C S <sowmya.srini09@gmail.com>
2020-09-30 14:39:34 +0530

    stock lookup form

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,25 @@
+class StocksController < ApplicationController
+  def search
+    def search
+      if params[:stock].present?
+        @stock = Stock.new_lookup(params[:stock])
+        if @stock
+          respond_to do |format|
+           format.js {render partial:}
+          end
+          
+        else
+          respond_to do |format|
+            flash.now[:alert] = "Please enter a valid symbol to search"
+            format.js { render partial: 'users/result' }
+          end
+        end    
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a symbol to search"
+          format.js { render partial: 'users/result' }
+        end
+      end
+    end
+  
+  end 
