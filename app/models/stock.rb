a337e2f4e91e02682ef963cf5d17d712c5e7c6a7
Sowmya C S <sowmya.srini09@gmail.com>
2020-09-30 17:16:58 +0530

    stock listing

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,11 @@
 class Stock < ApplicationRecord
 
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
+
+
   def self.new_lookup(ticker_symbol)
   client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                     endpoint: 'https://cloud.iexapis.com/v1')
@@ -8,5 +14,8 @@ class Stock < ApplicationRecord
           rescue => exception
           return nil
     end
+  end
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
